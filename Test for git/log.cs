24c65a1c1632488ab6ab2846ed00e9339ae1b685
Mehrad141 <mehradkhavary@gmail.com>
2023-05-20 09:07:47 -0700

    Adding log class for log series

---
+++ Test for git/log.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_for_git
+{
+    internal class log
+    {
+        public log(string message) { 
+        }
+    }
+}
